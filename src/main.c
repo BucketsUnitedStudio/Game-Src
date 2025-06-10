379943052e562a42e4604c4387064db4bb14ad12
ZonicTrout <chesskingmj@gmail.com>
2025-06-10 16:43:23 -0700

    Syncing repo

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Main window
+SDL_Window* global_Window = NULL;
+
+// Main Screen Surface
+SDL_Surface* global_Screen_Surface = NULL;
+
 
 int main() {
-    printf("Hello, World!\n");
+    SDL_Surface* initial_Surface = NULL;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "Was unable to initialize SDL: %s\n",
+                SDL_GetError());
+        exit(1);
+    }
 }
